7f13cd3efd29fb60b58f265841d89125e3a475b2
GavilanesJr <64985296+JorgeGavilanes@users.noreply.github.com>
2022-12-18 10:46:20 -0500

    getdatapost

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const port = process.env.PORT || 8080;
+
+app.use(bodyParser.json());
+
+app.post("/pedidonuevo", (req, res) => {
+    res.status(200).send("ok");
+    console.log("Nuevo pedido");
+    console.log(req.body);
+});
 
 app.get("/", (req, res) => {
     res.send("¡Hola mundo!");
     console.log("Acceding to content");
-});
-
-app.post("/pedidonuevo", (req, res) => {
-    res.send("ok").status(200);
-    console.log("Nuevo pedido");
 });
 
 app.listen(port, () => {
